31a0fbb556753062b82eb2898797c021ea57b10a
Zhenya Polyvanyi <iepoly@microsoft.com>
2024-12-11 12:57:26 +0000

    Reduce memory allocations in day 10 solution

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -32,8 +32,8 @@ fn main() {
     let mut result: Vec<Vec<(usize, usize)>> = Vec::new();
 
     for (x, y) in trailheads {
-        let mut route = vec![(x, y)];
-        if let Some(paths) = trailpaths(&trailmap, &mut route) {
+        let route = vec![(x, y)];
+        if let Some(paths) = trailpaths(&trailmap, route) {
             result.extend(paths);
         }
     }
@@ -51,48 +51,48 @@ fn main() {
     println!("Part 2: {}", result.len());
 }
 
-fn trailpaths(map: &[Vec<u8>], route: &mut [(usize, usize)]) -> Option<Vec<Vec<(usize, usize)>>> {
+fn trailpaths(map: &[Vec<u8>], route: Vec<(usize, usize)>) -> Option<Vec<Vec<(usize, usize)>>> {
     let (x, y) = route.last().copied().expect("Route is empty");
 
     let h = map[x][y];
     if h == 9 {
-        return Some(vec![route.to_owned()]);
+        return Some(vec![route]);
     }
 
     let mut result = Vec::new();
 
     // north
     if x > 0 && map[x - 1][y] == h + 1 {
-        let mut route = route.to_owned();
+        let mut route = route.clone();
         route.push((x - 1, y));
-        if let Some(paths) = trailpaths(map, &mut route) {
+        if let Some(paths) = trailpaths(map, route) {
             result.extend(paths);
         }
     }
 
     // east
     if y + 1 < map[x].len() && map[x][y + 1] == h + 1 {
-        let mut route = route.to_owned();
+        let mut route = route.clone();
         route.push((x, y + 1));
-        if let Some(paths) = trailpaths(map, &mut route) {
+        if let Some(paths) = trailpaths(map, route) {
             result.extend(paths);
         }
     }
 
     // south
     if x + 1 < map.len() && map[x + 1][y] == h + 1 {
-        let mut route = route.to_owned();
+        let mut route = route.clone();
         route.push((x + 1, y));
-        if let Some(paths) = trailpaths(map, &mut route) {
+        if let Some(paths) = trailpaths(map, route) {
             result.extend(paths);
         }
     }
 
     // west
     if y > 0 && map[x][y - 1] == h + 1 {
-        let mut route = route.to_owned();
+        let mut route = route.clone();
         route.push((x, y - 1));
-        if let Some(paths) = trailpaths(map, &mut route) {
+        if let Some(paths) = trailpaths(map, route) {
             result.extend(paths);
         }
     }
